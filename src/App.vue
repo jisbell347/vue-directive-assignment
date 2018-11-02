ec96e44600a76761d1efd4dddde3c252fa75c8ed
Joseph Isbell <shadowwolf347@comcast.net>
2018-11-02 11:36:30 -0600

    Created a custom eventlistener and attached it to a button/div.

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,39 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-custom:click="clicked" class="btn btn-primary">Click Me!</button>
+                <hr>
+                <div style="width: 100px; height: 100px; background-color: lightgreen"
+                        v-custom:mouseenter="mouseEnter"
+                        v-custom:mouseleave="mouseLeave"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            custom: {
+                bind(el, binding){
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
+            }
+        },
+        methods: {
+            clicked(){
+                alert('I was clicked!');
+            },
+            mouseEnter(){
+                console.log('Mouse entered!');
+            },
+            mouseLeave(){
+                console.log('Mouse Leave!');
+            }
+        }
+    }
 </script>
 
 <style>
